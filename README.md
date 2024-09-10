1b1b46cbaa74eb3bc42aa256c9f11340f2036eeb
JOTHSHANA <jothshana123@gmail.com>
2024-09-10 18:23:39 +0530

    deploy

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ShareMonitor
 A website for monitoring shared files.
+
+# template
+EMAIL_USER=your-email@gmail.com
+EMAIL_PASS=your-email-password
+CLIENT_ID=41363284832-na1ffm7pcpnvgm1fmv1ha13tm4k4g8i8.apps.googleusercontent.com
+CLIENT_SECRET=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
+CLIENT_URL=http://localhost:5173
+JWT_SECRET="this is my secret code"
+DB_HOST=localhost
+DB_USER=root
+DB_PASSWORD=root
+DB_NAME=share_manager
---
+++ share_manager (2).sql
@@ -0,0 +1,126 @@
+-- Adminer 4.8.1 MySQL 8.3.0 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `documents`;
+CREATE TABLE `documents` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `folder` bigint NOT NULL,
+  `pdf` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `video` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `link` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `general_doc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `status` enum('-1','0','1','3') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '1',
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `folder` (`folder`),
+  CONSTRAINT `documents_ibfk_1` FOREIGN KEY (`folder`) REFERENCES `folders` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `folders`;
+CREATE TABLE `folders` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `s_day` bigint NOT NULL,
+  `e_day` bigint NOT NULL,
+  `level` bigint NOT NULL,
+  `work_type` bigint NOT NULL,
+  `status` enum('-1','0','1','2') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '1',
+  `merge_random` bigint DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `level` (`level`),
+  KEY `work_type` (`work_type`),
+  CONSTRAINT `folders_ibfk_1` FOREIGN KEY (`level`) REFERENCES `levels` (`id`),
+  CONSTRAINT `folders_ibfk_2` FOREIGN KEY (`work_type`) REFERENCES `work_type` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `levels`;
+CREATE TABLE `levels` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `subject` int NOT NULL,
+  `level` bigint NOT NULL,
+  `lvl_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `status` enum('-1','0','1') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '1' COMMENT '1',
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `subject` (`subject`),
+  CONSTRAINT `levels_ibfk_1` FOREIGN KEY (`subject`) REFERENCES `subjects` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `resources`;
+CREATE TABLE `resources` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `icon_path` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `order_by` int NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `role_resources`;
+CREATE TABLE `role_resources` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `role_id` bigint NOT NULL,
+  `resource_id` bigint NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  KEY `resource_id` (`resource_id`),
+  CONSTRAINT `role_resources_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  CONSTRAINT `role_resources_ibfk_2` FOREIGN KEY (`resource_id`) REFERENCES `resources` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `subjects`;
+CREATE TABLE `subjects` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `status` enum('-1','0','1') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '1',
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `role_id` bigint NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `gmail` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `work_type`;
+CREATE TABLE `work_type` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `status` enum('0','1') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '1' COMMENT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- 2024-09-10 11:41:15
