576c1da2300125cae34a4db32b94484bd43bcb86
PsSkill <150027732+PsSkill@users.noreply.github.com>
2024-09-11 18:27:22 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -39,7 +39,7 @@ app.use(bodyParser.json());
 console.log(process.env.API)
 
 app.use(`${process.env.API}/api`, resources);
-app.use(`${process.env.API}/auth`, auth);
+app.use(`${process.env.API}/api/auth`, auth);
 app.use(`${process.env.API}/api`, subjectRoutes);
 app.use(`${process.env.API}/api`, levelRoutes);
 app.use(`${process.env.API}/api`, documentRoutes);
